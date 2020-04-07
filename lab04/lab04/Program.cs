56dac0495d4c10e7c6c45c198c99e67bddaf8f83
cveral <cavera2@miuandes.cl>
2020-04-07 16:10:09 -0400

    firt commit

---
+++ lab04/lab04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab04
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
